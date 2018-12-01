996d971511fd0da8e778daea909636a698c72f76
faisalvora12 <faisalvora12.fv@gmail.com>
2018-11-30 19:30:02 -0500

    commented get userId

--- main.js
+++ main.js
@@ -12,7 +12,8 @@ request.onreadystatechange = function () {
 };
 request.open('POST', 'get/'+localStorage.lastname, true);
 request.send();
-    
+});
+/*
 request.onreadystatechange = function () {
     if (request.readyState === 4 && request.status === 200) {
         alert("the user if is :"+request.response);
@@ -26,3 +27,4 @@ request.open('POST', 'get/userid/'+localStorage.lastname, true);
 request.send();
 });
 
+*/
