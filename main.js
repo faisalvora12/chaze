deb599ff9c4ec66ee3d8cd9c5ec1d8181f30c186
faisalvora12 <faisalvora12.fv@gmail.com>
2018-12-02 15:32:29 -0500

    adding an alert for local storage

--- main.js
+++ main.js
@@ -9,6 +9,7 @@ window.addEventListener('load', function() {
 var request = new XMLHttpRequest();
 request.onreadystatechange = function () {
     if (request.readyState === 4 && request.status === 200) {
+      alert(localStorage.lastname);
         document.getElementById("showuser").innerText=request.response;
     }
     else if(request.status === 404 && request.readyState===4)
