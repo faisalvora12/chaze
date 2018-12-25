ef027e49f1679072c9133eefcb1a78bd47b305d9
faisalvora12 <faisalvora12.fv@gmail.com>
2018-12-25 15:56:48 +0530

    html clear

--- main.js
+++ main.js
@@ -31,7 +31,7 @@ request.onreadystatechange = function () {
       var bs=0;
       var free=0;
       var str=request.response.split("!");
-      jQuery('#panels div').html('');
+      $('#panels div').empty();
       jQuery(document).ready(function(){
        $("#training").append("training number //backstroke   //   breaststroke   //    distperlength  //   freestyle<br>");
 });
