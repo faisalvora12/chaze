089ae4da459bc296422507d76425ebe866b01a7e
faisalvora12 <faisalvora12.fv@gmail.com>
2019-01-15 19:04:58 -0500

    changing " to ' in onclick

--- main.js
+++ main.js
@@ -71,7 +71,7 @@ $(document).ready(function(){
     document.getElementById(idp).style.transform = "translateY(4px)";//just gives the illution of a click
     document.getElementById(id).style.color = "red";
     document.getElementById("main").style.visibility = "hidden";
-     $("#nextpage").append("<button onclick="train()" type='button'>Click Me!</button>");
+     $("#nextpage").append("<button onclick='train()' type='button'>Click Me!</button>");
   });
   /*changes the color of the cliked option*/
   $("#train").click(function(){
