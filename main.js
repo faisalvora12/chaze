362566bdff8a68b0db988355debc13aee61df8fe
faisalvora12 <faisalvora12.fv@gmail.com>
2019-01-17 20:04:23 -0500

    testing a

--- main.js
+++ main.js
@@ -80,7 +80,8 @@ $(document).ready(function(){
     var training=text.split("//");
     var trainingid=training[1];
 /*********************************************************/    
-     var blobUri = 'https://' + 'chazestorage' + '.blob.core.windows.net';
+    var a=0; 
+    var blobUri = 'https://' + 'chazestorage' + '.blob.core.windows.net';
 var blobService = AzureStorage.Blob.createBlobServiceWithSas(blobUri, '?sv=2018-03-28&ss=b&srt=sco&sp=rwdlac&se=2119-01-18T08:03:19Z&st=2019-01-18T00:03:19Z&spr=https&sig=ZHPdLCVN2ylcrU0n07Xz16upTxCaiQRsl1SvvCHvEFk%3D');
 blobService.listContainersSegmented(null, function (error, results) {
     if (error) {
@@ -89,8 +90,10 @@ blobService.listContainersSegmented(null, function (error, results) {
         for (var i = 0, container; container = results.entries[i]; i++) {
             console.log(container);
         }
+     a=1;
     }
 });
+    alert(a);
     //getting data from blob storage
     /*request.onreadystatechange = function () {
     if (request.readyState === 4 && request.status === 200) {
