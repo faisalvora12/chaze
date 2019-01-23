1b0812137db00109849a91cb957168e61cef93ba
faisalvora12 <faisalvora12.fv@gmail.com>
2019-01-22 22:13:43 -0500

    adding blob code with module

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 //Database code
+const importblob = require('./azure-storage.blob.js');
+const importblob2 = require('./azure-storage.blob.min.js');
 
 var Connection = require('tedious').Connection;
 var Request = require('tedious').Request;
@@ -208,7 +210,8 @@ function gettrainingdata(userid,callback)
 //********************getting blob data**********************/
 function getblobdata(trainingdata,callback)
 {
- 
+var blobUri = 'https://' + 'chazestorage' + '.blob.core.windows.net';
+var blobService = AzureStorage.Blob.createBlobServiceWithSas(blobUri, '?sv=2018-03-28&ss=b&srt=sco&sp=rwdlac&se=2119-01-18T08:03:19Z&st=2019-01-18T00:03:19Z&spr=https&sig=ZHPdLCVN2ylcrU0n07Xz16upTxCaiQRsl1SvvCHvEFk%3D');
 
 }
 /**********************************************/
