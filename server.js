be1cd3f5c03a7c44c6e7a198cc3bb087c4d333f7
faisalvora12 <faisalvora12.fv@gmail.com>
2019-01-31 23:09:27 -0500

    removing blob1 and blob from blob code

--- server.js
+++ server.js
@@ -1,6 +1,4 @@
 //Database code
-const importblob = require('./azure-storage.blob.js');
-const importblob2 = require('./azure-storage.blob.min.js');
 
 var Connection = require('tedious').Connection;
 var Request = require('tedious').Request;
@@ -211,7 +209,7 @@ function gettrainingdata(userid,callback)
 function getblobdata(trainingdata,callback)
 {
 var blobUri = 'https://' + 'chazestorage' + '.blob.core.windows.net';
-var blobService = importblob2.AzureStorage.Blob.createBlobServiceWithSas(blobUri, '?sv=2018-03-28&ss=b&srt=sco&sp=rwdlac&se=2119-01-18T08:03:19Z&st=2019-01-18T00:03:19Z&spr=https&sig=ZHPdLCVN2ylcrU0n07Xz16upTxCaiQRsl1SvvCHvEFk%3D');
+var blobService = AzureStorage.Blob.createBlobServiceWithSas(blobUri, '?sv=2018-03-28&ss=b&srt=sco&sp=rwdlac&se=2119-01-18T08:03:19Z&st=2019-01-18T00:03:19Z&spr=https&sig=ZHPdLCVN2ylcrU0n07Xz16upTxCaiQRsl1SvvCHvEFk%3D');
 
 }
 /**********************************************/
