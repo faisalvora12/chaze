55dc8bb2b6c198ee5cc091684e8cd306bafde82d
faisalvora12 <faisalvora12.fv@gmail.com>
2019-03-16 14:40:18 -0400

    trying charts

--- main.js
+++ main.js
@@ -45,7 +45,24 @@ request.onreadystatechange = function () {
           if(str2[3]==='true')back++;
            if(str2[4]==='true')bs++;
            if(str2[6]==='true')free++;
-             $("#panels").append("<div id='dummy"+i+"' class='col-sm panel panel-default' style='background-color: white'><label id='"+i+"' class='panel-body classWithPad'> training "+i+"//"+str2[1]+"//"+str2[2]+" //   "+str2[3]+"   //    "+str2[4]+"   //    "+str2[5]+"   //    "+str2[6]+"</label></div>"); 
+          $("#panels").append("<canvas id='myChart' width='1600' height='900'></canvas>")   
+          // Our labels along the x-axis
+var years = [1500,1600,1700,1750,1800,1850,1900,1950,1999,2050];
+// For drawing the lines
+var africa = [86,114,106,106,107,111,133,221,783,2478];
+   var ctx = document.getElementById("myChart");
+var myChart = new Chart(ctx, {
+  type: 'line',
+  data: {
+    labels: years,
+    datasets: [
+      { 
+        data: africa
+      }
+    ]
+  }
+});
+          //$("#panels").append("<div id='dummy"+i+"' class='col-sm panel panel-default' style='background-color: white'><label id='"+i+"' class='panel-body classWithPad'> training "+i+"//"+str2[1]+"//"+str2[2]+" //   "+str2[3]+"   //    "+str2[4]+"   //    "+str2[5]+"   //    "+str2[6]+"</label></div>"); 
       });
       }
       jQuery(document).ready(function(){
