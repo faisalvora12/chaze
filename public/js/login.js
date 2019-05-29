7c4570a124b31ae6a87f0f0c80c0bcdfc8afdd70
faisalvora12 <faisalvora12.fv@gmail.com>
2019-05-29 15:19:33 -0400

    changing path in login

--- public/js/login.js
+++ public/js/login.js
@@ -18,12 +18,12 @@ function facebook(image,fname,email){
         request.onreadystatechange = function () {
             
             if (request.readyState === 4 && request.status === 200) {
-                location.replace('public/main.html');
+                location.replace('main.html');
             }
             else if(request.status === 404 && request.readyState===4)
             {
                        alert("should load main.html");
-                location.replace('public/main.html');
+                location.replace('main.html');
             }
             else if(request.status === 401 && request.readyState===4)
             {
@@ -124,7 +124,7 @@ function login() {
         var request = new XMLHttpRequest();
         request.onreadystatechange = function () {
             if (request.readyState === 4 && request.status === 200) {
-                location.replace('public/main.html');
+                location.replace('main.html');
             }
             else if(request.status === 404 && request.readyState===4)
             {
